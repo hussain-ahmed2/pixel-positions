44dfc5e4399b7fbccab76c76f89888158a6fe60d
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-04-09 16:20:26 +0600

    add image fallback on employer logo

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,3 +3,11 @@ import './bootstrap';
 import.meta.glob([
     '../images/**'
 ]);
+
+const logoImages = document.querySelectorAll('.image-logo');
+
+logoImages.forEach(image => {
+    image.onerror = function() {
+        image.src = `https://placehold.co/400/png?text=${image.dataset.name}`;
+    }
+});
--- resources/views/components/employer-logo.blade.php
+++ resources/views/components/employer-logo.blade.php
@@ -1,3 +1,3 @@
 @props(['employer', 'width' => 90])
 
-<img class="rounded-xl" src="{{ asset($employer->logo) }}" alt="">
+<img class="rounded-xl w-22 image-logo" src="{{ asset($employer->logo) }}" alt="" data-name="{{ $employer->name }}">
