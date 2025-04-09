251de92cc0628a9a00e5fd92945aa0ef28fa3c81
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-04-09 16:21:09 +0600

    add search bar on search page

--- resources/views/results.blade.php
+++ resources/views/results.blade.php
@@ -1,4 +1,11 @@
 <x-layout>
+
+    <section class="text-center py-6 mb-6">
+        <x-forms.form action='/search' class="mt-6">
+            <x-forms.input name="q" placeholder="Web Developer..." :label="false" :value="request('q')" />
+        </x-forms.form>
+    </section>
+
     <x-page-heading>Results</x-page-heading>
 
     <div class="space-y-6">
